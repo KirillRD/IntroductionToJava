89aacd2fc246de32e5532571259f3531737874fe
KirillRD <ryabov.kd@gmail.com>
2021-09-29 20:48:39 +0300

    Init

--- src/by/training/simplestapp/main/Main9.java
+++ src/by/training/simplestapp/main/Main9.java
@@ -19,15 +19,15 @@ public class Main9 {
     public static void main(String[] args) {
         int m1, m2, k;
 
-        m1 = inputIntFromConsole("m1");
+        m1 = inputIntFromConsole("length of mas1");
         while (m1 <= 0) {
             System.out.println("Wrong input. \"m1\" should be natural. Please, try again.");
-            m1 = inputIntFromConsole("m1");
+            m1 = inputIntFromConsole("length of mas1");
         }
-        m2 = inputIntFromConsole("m2");
+        m2 = inputIntFromConsole("length of mas2");
         while (m2 <= 0) {
             System.out.println("Wrong input. \"m2\" should be natural. Please, try again.");
-            m2 = inputIntFromConsole("m2");
+            m2 = inputIntFromConsole("length of mas2");
         }
         k = inputIntFromConsole("k");
         while (k <= 0 || k >= m1) {
@@ -38,21 +38,21 @@ public class Main9 {
         int[] mas1 = new int[m1];
 
         for (int i = 0; i < m1; i++) {
-            mas1[i] = inputIntFromConsole("a[" + i + "]");
+            mas1[i] = inputIntFromConsole("mas1[" + i + "]");
         }
 
         int[] mas2 = new int[m2];
 
-        for (int i = 0; i < (m2); i++) {
-            mas2[i] = inputIntFromConsole("a[" + i + "]");
+        for (int i = 0; i < m2; i++) {
+            mas2[i] = inputIntFromConsole("mas2[" + i + "]");
         }
 
         int[] mas = new int[m1 + m2];
 
         for (int i = 0; i < (m1 + m2); i++) {
-            if (i <= k) {
+            if (i <= (k - 1)) {
                 mas[i] = mas1[i];
-            } else if (i <= k + m2) {
+            } else if (i <= (k + m2 - 1)) {
                 mas[i] = mas2[i - k];
             } else {
                 mas[i] = mas1[i - m2];
