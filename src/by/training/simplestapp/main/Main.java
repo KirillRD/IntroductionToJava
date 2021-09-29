8c44b90450686769e2e996b60c6999808c41db61
KirillRD <ryabov.kd@gmail.com>
2021-09-29 19:05:55 +0300

    Initial commit

---
+++ src/by/training/simplestapp/main/Main.java
@@ -0,0 +1,52 @@
+package by.training.simplestapp.main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int inputIntFromConsole(String str) {
+        int value;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input " + str + ": ");
+        while (!sc.hasNextInt()) {
+            sc.nextLine();
+            System.out.println("Wrong input. Please, try again.");
+            System.out.print("Input " + str + ": ");
+        }
+        value = sc.nextInt();
+        return value;
+    }
+
+    public static void main(String[] args) {
+        int n, k;
+
+        while (true) {
+            n = inputIntFromConsole("n");
+            while (n <= 0) {
+                System.out.println("Wrong input. \"n\" should be natural. Please, try again.");
+                n = inputIntFromConsole("n");
+            }
+            k = inputIntFromConsole("k");
+            while (k <= 0) {
+                System.out.println("Wrong input. \"k\" should be natural. Please, try again.");
+                k = inputIntFromConsole("k");
+            }
+
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = inputIntFromConsole("a[" + i + "]");
+                while (a[i] <= 0) {
+                    System.out.println("Wrong input. a[" + i + "] should be natural. Please, try again.");
+                    a[i] = inputIntFromConsole("a[" + i + "]");
+                }
+            }
+
+            int sum = 0;
+            for (int i : a) {
+                if (i%k == 0) sum += i;
+            }
+
+            System.out.println("Sum: " + sum);
+        }
+    }
+}
